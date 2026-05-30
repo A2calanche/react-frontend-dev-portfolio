6d04421479070b0b1887ca6aadab75b481a3b661
A2calanche <146496251+A2calanche@users.noreply.github.com>
2026-05-30 16:59:51 +0000

    Updates

---
+++ precache-manifest.2249ed28e6a33fc43f17fc3d1b536979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8bafeec830e486ae8bfd4861432a40",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "db397a303c7880c6f3fa",
+    "url": "/react-frontend-dev-portfolio/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "773aee2a8187f0810f68",
+    "url": "/react-frontend-dev-portfolio/static/css/main.df700d1f.chunk.css"
+  },
+  {
+    "revision": "db397a303c7880c6f3fa",
+    "url": "/react-frontend-dev-portfolio/static/js/2.7b164398.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/react-frontend-dev-portfolio/static/js/2.7b164398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773aee2a8187f0810f68",
+    "url": "/react-frontend-dev-portfolio/static/js/main.af74ea8f.chunk.js"
+  },
+  {
+    "revision": "0c71dbb3b6aa4f86725d",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.89d7af9d.js"
+  }
+]);
