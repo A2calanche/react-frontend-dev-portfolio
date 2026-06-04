374680c659e8cc81d593f91622d1479f1a3dc590
A2calanche <146496251+A2calanche@users.noreply.github.com>
2026-06-04 00:13:09 +0000

    Updates

--- precache-manifest.b625a498a5f58c86ee532ce16426eba4.js
+++ precache-manifest.b625a498a5f58c86ee532ce16426eba4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "607a259cece40d0eeb90b53a28132183",
+    "revision": "2dc7614da98b8ed2836d7e31b8d7f02a",
     "url": "/react-frontend-dev-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-frontend-dev-portfolio/static/css/2.fd277b09.chunk.css"
   },
   {
-    "revision": "50da95c63b996602f096",
+    "revision": "2d4e6a236b64a0b4dfdb",
     "url": "/react-frontend-dev-portfolio/static/css/main.df700d1f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-frontend-dev-portfolio/static/js/2.5ee7a6a7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "50da95c63b996602f096",
-    "url": "/react-frontend-dev-portfolio/static/js/main.86be4244.chunk.js"
+    "revision": "2d4e6a236b64a0b4dfdb",
+    "url": "/react-frontend-dev-portfolio/static/js/main.b3ec1d9e.chunk.js"
   },
   {
     "revision": "0c71dbb3b6aa4f86725d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.c2ad3f5fb1ad57dc8c17bd7e53697272.js"
+  "/react-frontend-dev-portfolio/precache-manifest.b625a498a5f58c86ee532ce16426eba4.js"
 );
 
 self.addEventListener('message', (event) => {
